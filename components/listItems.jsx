9160a58caaad67b9164fbf705ff1f86a0a22c87e
murlipillamari6 <murlipillamari6@gmail.com>
2022-05-17 09:08:01 -0700

    DataKeeper Application completed

---
+++ components/listItems.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function ListItems(props) {
+    const {task,addTask,setText}=props;
+   
+    const deleteItems = (index) => {
+        const updatedItems = task.filter((item, ind) => {
+          return ind !== index;
+        });
+        addTask(updatedItems);
+      };
+      const editData = (index) => {
+        const editData = task.find((items, ind) => {
+          if (ind === index) {
+            return items;
+          }
+        });
+        setText(editData);
+        const updatedItems = task.filter((item, ind) => {
+          return ind !== index;
+        });
+        addTask(updatedItems);
+      };
+  return (
+    <>
+      <div className="-ml-3">
+        {task.map((items, index) => {
+          return (
+            <div
+              key={index}
+              className="flex md:w-1/4 w-3/4 mx-auto justify-between bg-blue-500 hover:bg-white text-white hover:text-black mt-6 rounded-md"
+            >
+              <button
+                className="w-full mx-auto  transition-all font-thin text-left  px-4 py-2 text-lg outline-none cursor-pointer"
+                title="Edit"
+                onClick={() => {
+                  editData(index);
+                }}
+              >
+                {/* <span className="pr-4 border-r-2">{index + 1}</span>&nbsp;&nbsp; */}
+                {items}
+              </button>
+              <button
+                className=" px-4 py-2 rounded-md cursor-pointer outline-none text-2xl"
+                title="Delete"
+                onClick={(e) => deleteItems(index)}
+              >
+                -
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import ListItems from "../components/listItems";
+export default function Home() {
+  const [task, addTask] = useState([]);
+  const [text, setText] = useState("");
+
+  const addItems = () => {
+    text && addTask([...task, text]);
+    setText("");
+  };
+
+  return (
+    <>
+      <div className="h-screen" style={{ background: "#563d6e" }}>
+        <div className="text-center pt-20 ">
+          <h2 className="text-white text-3xl">DataKeeper</h2>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Add Item..."
+            className="md:w-1/4 w-3/4 border px-4 mt-8 py-2 rounded-md text-lg outline-none "
+            required
+          />
+          <button
+            className="bg-white relative right-7 cursor-pointer outline-none text-2xl"
+            onClick={addItems}
+          >
+            +
+          </button>
+          <div className={`${task.length > 0 ? "block mt-10" : "hidden"} `}>
+            <ListItems
+              task={task}
+              addTask={addTask}
+              text={text}
+              setText={setText}
+            />
+
+            <button
+              className="mt-10 hover:bg-blue-500 bg-white px-10 hover:text-white transition-all text-base  py-2 uppercase outline-white"
+              onClick={() => {
+                addTask([]);
+              }}
+            >
+              Clear All
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}

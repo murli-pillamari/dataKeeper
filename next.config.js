b510fd672cf700a9c547f013416b278a35fda3c8
Murli Pillamari <70046894+murli-pillamari@users.noreply.github.com>
2022-05-20 06:50:27 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/ap-grid-layout": { page: "/ap-grid-layout" },
+    }
+  },
+  assetPrefix: !debug ? 'https://mrp-io.github.io/Next-React-Components/' : '',
+}
